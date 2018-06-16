8592d4d2b41a49d7721f123cc8f29e835a5183a9
Tonitch <tonitch@herisson.ovh>
2018-06-16 22:18:29 +0200

    console formating

--- jeux/__init__.py
+++ jeux/__init__.py
@@ -1,7 +1,11 @@
 #import all .py file in this directory
 import os
+print("===Loading Modules===")
 for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
+    print("==> " + module[:-3], end="")
     __import__(module[:-3], locals(), globals())
+    print("   [Done]")
+print("=====================")
 del module
