98525ccc39def14e4f3e0bfc15febfebad08fb38
Tonitch <tonitch@herisson.ovh>
2018-06-16 11:59:44 +0200

    adding jeux/ folder + command ping + commmand préfix in config file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Imports
 import discord
+from discord.ext import commands
+import jeux
 import asyncio
 import configparser
 
@@ -7,12 +9,18 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 bot_key = config.get("Bot", "key") #bot's key
+pcmd = config.get("Bot", "Command_Prefix") #command s'Prefix
 
 #bot client
-Client = discord.Client()
+Client = commands.Bot(command_prefix=pcmd) 
 
 @Client.event
 async def on_ready():
-    print('Starting Bot!')
+    print("Starting Bot!")
+    print("Bot Id = " + Client.user.id)
+
+@Client.command()
+async def ping():
+    await Client.say("Pong!")
 
 Client.run(bot_key)
